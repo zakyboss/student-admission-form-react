ae3574ff68c22740aaa7a1821ff2fb569e332090
Zakyboss <Boochimo8@gmail.com>
2025-01-16 06:13:38 +0300

    Recieving all components and rendering them

--- admission-form-react-state/src/App.js
+++ admission-form-react-state/src/App.js
@@ -1,8 +1,15 @@
 import Header from "./Header";
+import Footer from "./Footer";
+import StudentList from "./StudentList";
+import Form from "./Form";
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Form/>
+        <StudentList/>
+     <Footer/>
+     
     </div>
   );
 }
